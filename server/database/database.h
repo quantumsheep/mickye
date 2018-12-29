2f3fef6db296dd32859044eeb2fb0ea9fcb4bf3d
QuantumSheep <nathanael.dmc@outlook.fr>
2018-12-29 15:45:34 +0100

    Rework and GUI

--- server/database/database.h
+++
@@ -1,3 +0,0 @@
-#include "sqlite3.h"
-
-int open(sqlite3 *db);
---
+++ server/gui/gui.h
@@ -0,0 +1,5 @@
+#include <gtk/gtk.h>
+
+#define BUILDER_PATH "builder.ui"
+
+void add_handler(GtkBuilder *builder, char *id, char *on, void *callback);
