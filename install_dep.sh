fe2ec5b29653e1144d8a77b557daf6d6e01b6279
QuantumSheep <nathanael.dmc@outlook.fr>
2018-12-30 20:45:09 +0100

    Add a dependencies installation script

---
+++ install_dep.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script installs all the dependencies required for MICKYE
+# It has been tested with apt
+#
+# It installs the following packages:
+#   - libgtk-3-dev          --> GTK3 SDK
+#   - libsqlite3-dev        --> SQLite 3 SDK
+
+if [ "$(id -u)" != "0" ]; then
+    echo "You must start this script with root privileges"
+    exit 1
+fi
+
+apt-get install \
+    libgtk-3-dev \
+    libsqlite3-dev
