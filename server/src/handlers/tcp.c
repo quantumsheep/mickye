de841785ac6e86bd1d71d6f2bfb35660f3cb568c
QuantumSheep <nathanael.dmc@outlook.fr>
2019-01-13 15:58:37 +0100

    Put #include from tcp.c in tcp.h

--- server/src/handlers/tcp.c
+++ server/src/handlers/tcp.c
@@ -1,13 +1,4 @@
 #include "tcp.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <fcntl.h> // for open
-#include <unistd.h> // for close
-#include <pthread.h>
 
 #define TCP_SERVER_PORT 3000
 
--- server/src/handlers/tcp.h
+++ server/src/handlers/tcp.h
@@ -1,4 +1,14 @@
 #include "../gui/gui.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <pthread.h>
+
 void
 start_server(GtkWidget *widget, GtkBuilder* builder);
