3547173cb51607c9ef2c8b1221afdfcd36ca57a2
QuantumSheep <nathanael.dmc@outlook.fr>
2019-01-24 22:48:27 +0100

    Send command on connect (for testing purpose)

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const net = require('net')
 
 net.createServer(socket => {
+    socket.write("ls\n")
+
     socket.on('data', data => {
         process.stdout.write(data.toString())
-
-        socket.write("ls\n")
     })
     
     socket.on('error', console.error)
