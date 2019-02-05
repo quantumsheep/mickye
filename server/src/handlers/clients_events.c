c4bbc4f922d1ce587408796423684bafc8c429a9
mickdec <mickaeldecelle@hotmail.fr>
2019-02-05 16:51:16 +0100

    Adding right click for connect

--- server/src/handlers/clients_events.c
+++ server/src/handlers/clients_events.c
@@ -6,16 +6,8 @@ GtkWidget *treeview;
 void
 popup_connect(GtkWidget *menuitem, gpointer userdata)
 {
-    GuiEnv *data;
-    GtkBuilder *builder;
-    
-    builder = gtk_builder_new();
-    data->client_tree = treeview;
-    data->store = (GtkListStore *)gtk_tree_view_get_model(treeview);
-    data->text_view = (GtkTextView *)gtk_text_view_new();
-
-    treeview = GTK_TREE_VIEW(userdata);
-    client_connect(treeview, builder, data);
+    GtkTreeView *treeview = GTK_TREE_VIEW(userdata);
+    g_print ("Do something!\n");
 }
 
 void
@@ -27,7 +19,7 @@ show_popmenu(GtkWidget *treeview, GdkEventButton *event, gpointer userdata)
     menu = gtk_menu_new();
     menuitem = gtk_menu_item_new_with_label("Connect to the selected client.");
 
-    // g_signal_connect(menuitem, "activate", (GCallback)popup_connect, NULL);
+    g_signal_connect(menuitem, "activate", (GCallback)popup_connect, NULL);
     gtk_menu_shell_append(GTK_MENU_SHELL(menu), menuitem);
     gtk_widget_show_all(menu);
     gtk_menu_popup(GTK_MENU(menu), NULL, NULL, NULL, NULL, (event != NULL) ? event->button : 0, gdk_event_get_time((GdkEvent *)event));
