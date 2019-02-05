d5781b085a88a109546be58885d524eb46306d44
QuantumSheep <nathanael.dmc@outlook.fr>
2019-02-05 14:37:36 +0100

    Clean fd.h (Client side)

--- client/src/fd.h
+++ client/src/fd.h
@@ -1,9 +1,9 @@
 #ifndef _FD_H
 #define _FD_H
 
-#define FD_CHUNK_SIZE ((size_t)8192)
-
 #include <unistd.h>
+
+#define FD_CHUNK_SIZE ((size_t)8192)
 
 #define fd_read(fd, data) read(fd, data, FD_CHUNK_SIZE)
 #define fd_write(fd, data) write(fd, data, strlen(data))
