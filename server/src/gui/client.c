2bbdb37ae2e5d0519f8aa0bd6fafa12d0ccbc9e2
mickdec <mickaeldecelle@hotmail.fr>
2019-02-06 13:15:57 +0100

    Adding set isconnect function

--- server/src/gui/client.c
+++ server/src/gui/client.c
@@ -1,5 +1,6 @@
 #include "client.h"
 
+//Declaring collumns enumerator for the Store
 enum
 {
     COL_NAME = 0,
@@ -9,12 +10,84 @@ enum
     NUM_COLS
 };
 
+/*
+*   Function to set a client disconnected in the ListStore (GtkListStore) corresponding to the list of clients
+*   Asking for :
+*   @param      *id         socket of the client
+*   @param      *data       GuiEnv data set in main
+*/
+gboolean
+client_set_disconnect(int id, GuiEnv *data)
+{
+    GtkTreeIter iter;
+    GtkTreeModel *model;
+    GtkTreeView *client_tree;
+    GValue value = G_VALUE_INIT;
+    gboolean valid;
+    gboolean found;
+    char *name;
+    char *ipv4;
+    char *status;
+    int socketmp;
+
+    //Setting the status to disconnected
+    status = "Disconnected";
+
+    //Setting the base tree and model of the client list
+    client_tree = GTK_TREE_VIEW(data->client_tree);
+    model = gtk_tree_view_get_model(client_tree);
+
+    //Running a search for each element in the client tree, if the seacrhed socket exist, set the iter. else return false;
+    found = FALSE;
+    while (!found)
+    {
+        valid = gtk_tree_model_get_iter_first(model, &iter);
+        gtk_tree_model_get_value(model, &iter, COL_SOCKET, &value);
+        socketmp = g_value_get_int(&value);
+        g_value_unset(&value);
+        while (valid)
+        {
+            if(socketmp == id){
+                found = TRUE;
+                break;
+            }else{
+                valid = gtk_tree_model_iter_next(model, &iter);
+            }
+        }
+        return FALSE;
+    }
+
+    if (gtk_tree_selection_get_selected(selection, &model, &iter))
+    {
+        gtk_tree_model_get_value(model, &iter, COL_NAME, &value);
+        name = g_value_dup_string(&value);
+        g_value_unset(&value);
+        gtk_tree_model_get_value(model, &iter, COL_IPV4, &value);
+        ipv4 = g_value_dup_string(&value);
+        g_value_unset(&value);
+
+        gtk_list_store_remove((GtkListStore *)data->store, &iter);
+        gtk_list_store_insert_with_values(data->store, &iter, -1, COL_NAME, name, COL_SOCKET, id, COL_IPV4, ipv4, COL_STATUS, status, -1);
+
+        return TRUE;
+    }
+    return FALSE;
+}
+
+/*
+*   Function to add client in the ListStore (GtkListStore) corresponding to the list of clients
+*   Asking for :
+*   @param      *store      the main ListStore of the client list window
+*   @param      *client     a TcpClient object corresponding of a TcpClient struct
+*   @param      status      a int corresponding to the status of the client (connected, disconnected...)
+*/
 void
 client_add(GtkListStore *store, TcpClient *client, int status)
 {
     GtkTreeIter iter;
     char *status_str;
 
+    //status can correspond to a typedef status
     switch (status)
     {
     case CLIENT_CONNECTED:
@@ -28,11 +101,19 @@ client_add(GtkListStore *store, TcpClient *client, int status)
         break;
     }
 
+    //inserting the new row with sets values
     gtk_list_store_insert_with_values(store, &iter, -1, COL_NAME, "", COL_SOCKET, client->socket, COL_IPV4, client->ipv4, COL_STATUS, status_str, -1);
 }
 
+/*
+*   Function to connect with a client in the ListStore (GtkListStore) corresponding to the list of clients
+*   Asking for :
+*   a unused widget (fault is going to gui_add_handler function...) can be NULL
+*   a unused builder (fault is going to gui_add_handler function...) can be NULL
+*   a int corresponding to the status of the client (connected, disconnected...)
+*/
 void
-client_connect(GtkWidget *_, GtkBuilder *builder, GuiEnv *data)
+client_connect(GtkWidget *_, GtkBuilder *__, GuiEnv *data)
 {
     GtkTreeSelection *selection;
     GtkTreeIter iter;
