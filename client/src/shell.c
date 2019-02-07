5763131f039db3fbc85e8421fa13e88a98a8d2a4
QuantumSheep <nathanael.dmc@outlook.fr>
2019-02-07 15:49:03 +0100

    Set STDOUT to uppercase in shell.c

--- client/src/shell.c
+++ client/src/shell.c
@@ -37,7 +37,7 @@ shell_open()
         close(child2parent_fd[0]);
 
         /**
-         * Bind process STDIN and STDOUt to useful file descriptors used by the
+         * Bind process STDIN and STDOUT to useful file descriptors used by the
          * parent's
          */
         dup2(parent2child_fd[0], STDIN_FILENO);
